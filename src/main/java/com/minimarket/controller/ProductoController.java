b31939d292b457b8314d2e43a6b8a75eb3709059
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-06-08 19:38:16 -0400

    Add role-based access control to controllers using PreAuthorize annotations

--- src/main/java/com/minimarket/controller/ProductoController.java
+++ src/main/java/com/minimarket/controller/ProductoController.java
@@ -4,12 +4,14 @@ import com.minimarket.entity.Producto;
 import com.minimarket.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/productos")
+@PreAuthorize("hasAnyRole('EMPLEADO','GERENTE')")
 public class ProductoController {
 
     @Autowired
